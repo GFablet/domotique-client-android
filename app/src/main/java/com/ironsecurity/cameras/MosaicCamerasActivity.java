926266449a1bfcf200c6faa78baa9e33bf570f37
Manon <48240249+MeG1Emi@users.noreply.github.com>
2019-04-10 09:56:38 +0200

    Ajout Package/Activity Cameras

---
+++ app/src/main/java/com/ironsecurity/cameras/MosaicCamerasActivity.java
@@ -0,0 +1,13 @@
+package com.ironsecurity.cameras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MosaicCamerasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mosaic_cameras);
+    }
+}
