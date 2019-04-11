bcb2a5cf4d8d898cf0f95202ab953a9c467a600f
Guillaume <gfablet@gmail.com>
2019-04-11 10:06:29 +0100

    Ajout des tags lumieres salon

--- app/src/main/java/com/ironsecurity/operate_house/RoomActivity.java
+++ app/src/main/java/com/ironsecurity/operate_house/RoomActivity.java
@@ -21,6 +21,10 @@ public class RoomActivity extends AppCompatActivity {
     Button buttonClim;
     CheckBox checkBox;
 
+    int[] lumieresID = {R.id.checkBoxLampe1,R.id.checkBoxLampe2,R.id.checkBoxLampe3,
+                        R.id.checkBoxLampe4,R.id.checkBoxLampe5,R.id.checkBoxLampe6,
+                        R.id.checkBoxLampe7, R.id.checkBoxLampe8};
+    int[] voletsID = {R.id.checkBoxVolet1,R.id.checkBoxVolet2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +32,23 @@ public class RoomActivity extends AppCompatActivity {
         setContentView(R.layout.salon_layout);
 
 
+        for(int lumiere : lumieresID)
+        {
+            CheckBox checkBox = findViewById(lumiere);
 
-        checkBox = findViewById(R.id.radioButtonLampe1);
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if(isChecked)
+                    {
+                        Toast.makeText(RoomActivity.this, buttonView.getTag() + " Checked", Toast.LENGTH_SHORT).show();
+                    } else
+                        Toast.makeText(RoomActivity.this, buttonView.getTag() + " Unchecked", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        /*
         checkBox.setChecked(true);
 
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -41,7 +60,7 @@ public class RoomActivity extends AppCompatActivity {
                 } else
                     Toast.makeText(RoomActivity.this, "Unchecked", Toast.LENGTH_SHORT).show();
             }
-        });
+        });*/
 
     }
 }
