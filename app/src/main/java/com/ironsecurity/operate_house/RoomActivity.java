362bba95210e51ad8b890c5fd2c05410590a2d8f
Guillaume <gfablet@gmail.com>
2019-04-11 09:43:06 +0100

    modifications layout salon
    changement des icones, et des boutons en checkbox

--- app/src/main/java/com/ironsecurity/operate_house/RoomActivity.java
+++ app/src/main/java/com/ironsecurity/operate_house/RoomActivity.java
@@ -4,9 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.ironsecurity.MainActivity;
 import com.ironsecurity.R;
@@ -17,39 +19,29 @@ public class RoomActivity extends AppCompatActivity {
     Button buttonLumiere1;
     Button buttonVolet1;
     Button buttonClim;
-    RadioButton radio;
+    CheckBox checkBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.salon_layout);
 
-        buttonLumiere1 = findViewById(R.id.buttonLumiere1);
-        buttonVolet1 = findViewById(R.id.buttonVolet1);
 
-        //radio = findViewById(R.id.radioButton);
 
-        radio.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        checkBox = findViewById(R.id.radioButtonLampe1);
+        checkBox.setChecked(true);
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                if(isChecked)
+                {
+                    Toast.makeText(RoomActivity.this, "Checked", Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(RoomActivity.this, "Unchecked", Toast.LENGTH_SHORT).show();
             }
         });
-
-        buttonLumiere1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity.clientThread.sendMessage("operateOneEquipment/"+v.getTag().toString()+"/0");
-            }
-        });
-        buttonVolet1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity.clientThread.sendMessage("operateOneEquipment/"+v.getTag().toString()+"/0");
-            }
-        });
-
-
 
     }
 }
